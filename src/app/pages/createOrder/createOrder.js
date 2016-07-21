1dcb15891124bb54c44b46f8349ca4c5c3f94755
197203392@qq.com <wuenyu1!>
2016-07-22 00:18:01 +0800

    清酒，提现，账号

--- src/app/pages/createOrder/createOrder.js
+++ src/app/pages/createOrder/createOrder.js
@@ -24,7 +24,7 @@ export default angular.module('createOrder',[ionic])
 
 
 class CreateOrderController {
-    constructor ($ionicPopup,$scope,getOrderInfo,$ionicLoading,resourcePool,$stateParams,application) {
+    constructor ($ionicPopup,$scope,getOrderInfo,$ionicLoading,resourcePool,$stateParams,application,$ionicModal) {
         "ngInject"
         this.name = 'createOrder';
         this.payWays = 0; // 0:余额支付  1:支付宝   2:微信支付
@@ -39,49 +39,58 @@ class CreateOrderController {
         this.orderId = this.orderInfo.head;
         this.payId = this.orderInfo.id;
         this.application = application;
+        this.ionicModal = $ionicModal;
+        let t = this;
+        $scope.$on('password.confirm',function(event,args){
+            $scope.passowrd = args;
+            t.confirm();
+        })
     }
     changePay(type){
         this.payWays = type;
     }
-
     toPay(){
-        let t = this;
-        var $loading = t.loading;
-        t.popup.show({
-            title:'输入支付密码',
-            template:'<span class="barName" style="color:#A7264D;font-size:.34rem">{{vm.orderInfo.barinfo.name}}酒吧</span><p class="order-price" style="font-size:.32rem;">{{vm.orderInfo.money | currency:"￥"}}</p><codebox password="password"></codebox>',
-            buttons:[{
-                text:'确定',
-                onTap:function(){
-                    if(this.scope.password =='131646'){
-                        t.resourcePool.setOrderPay.request({
-                            id:t.payId
-                        }).then(res=>{
-                            var appInfo = JSON.parse(res.data.info);
-                            t.application.wechatPay(appInfo,()=>{
-                                $loading.show({
-                                    template:'支付成功',
-                                    duration:1000
-                                });
+        let $modal = this.ionicModal;
+        let $scope = this.scope;
 
-                            },()=>{
-                                $loading.show({
-                                    template:'支付失败或取消',
-                                    duration:1000
-                                })
-                            })
-
-                        });
-                        return true;
-                    }else{
-                        $loading.show({
-                            template:'支付密码错误',
-                            duration:1000
-                        })
-                    }
-                }
-            },{text:'取消'}],
-            scope:t.scope
-        })
+        $scope.modal = $modal.fromTemplate('<password on-hide="vm.hideModal()" password="password" ></password>',{
+            scope: $scope,
+            animation: 'slide-in-up'
+        });
+        $scope.modal.show();
     }
+
+    hideModal(){
+        this.scope.modal.hide();
+    }
+    confirm(){
+        let resourcePool = this.resourcePool;
+        let $loading = this.loading;
+        let t = this;
+        /**
+         * 微信支付
+         */
+        if(t.payWays == 2){
+            resourcePool.setOrderPay.request({
+                id:t.payId
+            }).then(res=>{
+                var appInfo = JSON.parse(res.data.info);
+                t.application.wechatPay(appInfo,()=>{
+                    $loading.show({
+                        template:'支付成功',
+                        duration:1000
+                    });
+                },()=>{
+                    $loading.show({
+                        template:'支付失败或取消',
+                        duration:1000
+                    })
+                })
+
+            });
+        }
+
+    }
+
+
 }
