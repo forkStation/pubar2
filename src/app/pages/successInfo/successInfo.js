729ea18e466efeff97a7465de2be621d14f27cc6
enyu <197203392@qq.com>
2016-08-02 18:27:15 +0800

    支付

---
+++ src/app/pages/successInfo/successInfo.js
@@ -0,0 +1,54 @@
+import tpl from './successInfo.jade'
+import './successInfo.scss'
+import { angular, ionic } from 'library'
+
+export default angular.module('successInfo',[ionic])
+    .config(function ($stateProvider) {
+        "ngInject"
+        $stateProvider
+            .state('successInfo', {
+                url: '/successInfo/:orderid/:genre?partyid',
+                controllerAs: 'vm',
+                controller: SuccessInfoController,
+                template: tpl(),
+                resolve:{
+                    orderInfo:function(resourcePool,$stateParams){
+                        let genre = $stateParams.genre;
+                        if(genre == 0){
+                            return resourcePool.getConfirmOrderInfo.request({
+                                id:$stateParams.orderid,
+                                genre:$stateParams.genre
+                            })
+                        }
+                        if(genre == 1){
+                            return resourcePool.getConfirmOrderInfo.request({
+                                genre:$stateParams.genre,
+                                partyid:$stateParams.partyid
+                            })
+                        }
+                    }
+                }
+            })
+    });
+
+
+class SuccessInfoController {
+    constructor (orderInfo,application,$stateParams) {
+        "ngInject"
+        this.name = 'successInfo';
+        this.application = application;
+        this.orderInfo = orderInfo.data.info;
+        this.stateParams = $stateParams;
+        this.headHost = application.headHost;
+        this.amount = ~~this.orderInfo.partyinfo.boyCount + ~~this.orderInfo.partyinfo.girlCount
+    }
+
+
+    backGroup(){
+        window.location.replace('/myGroup')
+    }
+    backBar(){
+        let _this = this;
+        window.location.replace('/barDetail/'+_this.orderInfo.barinfo.id)
+    }
+}
