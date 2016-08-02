f1c161ed55ddefec8c9aca96373d6a7fb0df96cc
197203392@qq.com <wuenyu1!>
2016-08-02 19:58:48 +0800

    sign

--- src/app/service/application.js
+++ src/app/service/application.js
@@ -99,9 +99,9 @@ export default function ( resourcePool, $q,$ionicPopup,$location) {
                 wx.config({
                     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                     appId: appInfo['appId'], // 必填，公众号的唯一标识
-                    timestamp:appInfo.timestamp.toString() , // 必填，生成签名的时间戳
+                    timestamp:appInfo.timestamp , // 必填，生成签名的时间戳
                     nonceStr: appInfo.nonceStr, // 必填，生成签名的随机串
-                    signature: appInfo.signType,// 必填，签名，见附录1
+                    signature: appInfo.signature,// 必填，签名，见附录1
                     jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
                 });
             })
